f410dd98794c0d4d90fe827ffaf411d320b9ee85
PedroCapivara <152212470+PedroCapivara@users.noreply.github.com>
2024-10-03 12:12:15 -0300

    Criando a função da barra de busca.
    Para poder criar, eu alterei o HTML, adicionando 2 atributos com o " data- " um para o nome dos produtos e o outro sua categoria.
    No JS esta mais detalhado o processo.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,10 +3,28 @@ let boxBuscar = document.querySelector(".buscar-box");
 let lupaa = document.querySelector('.lupa');
 let xFechar = document.querySelector(".fechar");
 
-lupaa.addEventListener('click',()=> {
+lupaa.addEventListener('click',()=> { // Abrir a barra de pesquisa //
     boxBuscar.classList.add('ativar')
 })
 
-xFechar.addEventListener("click", () => {
+xFechar.addEventListener("click", () => { // Fechar a barra de pesquisa //
   boxBuscar.classList.remove("ativar");
 });
+
+// Faz isso funcionar //
+let inputBusca = document.getElementById('input-busca');  // Caixa de pesquisa //
+let produtos = document.querySelectorAll('.produto');     // Uma lista com os produtos da classe: .produto //
+
+inputBusca.addEventListener('input', function(){   // Criando uma evento ao escrever //
+  let valorBusca = inputBusca.value.toLowerCase(); // Oque eu escrevo, transfomado em minusculas //
+
+  produtos.forEach(function(produto){ // O que eu entendi, que eu criei uma lista (NodeList) e o forEach vai ler essa lista //
+    nomeProduto = produto.getAttribute('data-nome').toLowerCase(); 
+
+    if (nomeProduto.includes(valorBusca)) { // Fazendo os produtos aparecer e esconder os não pesquisados //
+      produto.style.display = "block";
+    } else {
+      produto.style.display = "none";
+    }
+  })
+})

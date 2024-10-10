0946ed8082daed92fc9c9f1435698ba01c70624f
PedroCapivara <152212470+PedroCapivara@users.noreply.github.com>
2024-10-10 15:36:42 -0300

    Modo escuro.
    VIVA AS CAPIVARIAS

--- assets/js/modonot.js
+++ assets/js/modonot.js
@@ -1,1 +1,5 @@
-// Modo nortuno
+// Modo nortuno // 
+document.getElementById('tema').addEventListener('click', function(){
+    document.getElementById('produtos').classList.toggle('noturno');
+    document.querySelector('header').classList.toggle('noturno');
+});
